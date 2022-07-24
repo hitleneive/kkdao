2118286fd3f086247a83e20950a32f474620bc32
Duc Thai <dbom.1006@gmail.com>
2022-07-25 00:33:37 +0700

    Fix font load low

--- src/pages/PageWrapper/PageWrapper.js
+++ src/pages/PageWrapper/PageWrapper.js
@@ -163,6 +163,9 @@ const PageWrapper = () => {
             {(isSmallDesktop || isMobile) && <Footer />}
             {(isSmallDesktop || isMobile) && <ToTop />}
           </div>
+          <div className="load-font-1" />
+          <div className="load-font-2" />
+          <div className="load-font-3" />
         </div>
       )}
     </>
